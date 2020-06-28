53efd7007b82e1ba1d09bb7e3af5b89a8eb5c70a
juliomchado <juliocarlos00@hotmail.com>
2020-06-28 17:50:15 +0100

    first register created

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express';
+
 import appointmentsRouter from './appointment.routes'
+import usersRouter from './users.routes'
 
 const routes = Router();
 
 routes.use('/appointments', appointmentsRouter)
+routes.use('/users', usersRouter)
 
 
 export default routes;
