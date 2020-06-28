970f7d95f7e506fc123f491a97a14e940f908fb9
juliomchado <juliocarlos00@hotmail.com>
2020-06-28 02:27:05 +0100

    Repository typeORM

--- backend/src/database/index.ts
+++ backend/src/database/index.ts
@@ -1,5 +1,3 @@
 import { createConnection } from 'typeorm'
 
 createConnection();
-
-
--- backend/src/database/migrations/1593122942319-CreateAppointments.ts
+++ backend/src/database/migrations/1593122942319-CreateAppointments.ts
@@ -12,6 +12,7 @@ export class CreateAppointments1593122942319 implements MigrationInterface {
             type: 'varchar',
             isPrimary: true,
             generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()'
           },
           {
             name: 'provider',
