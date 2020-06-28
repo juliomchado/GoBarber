045d75515eab6123a996b4a9e931b16fd9d1d802
juliomchado <juliocarlos00@hotmail.com>
2020-06-28 16:14:37 +0100

    users table and models created

--- backend/src/database/migrations/1593122942319-CreateAppointments.ts
+++ backend/src/database/migrations/1593122942319-CreateAppointments.ts
@@ -17,12 +17,20 @@ export class CreateAppointments1593122942319 implements MigrationInterface {
           {
             name: 'provider',
             type: 'varchar',
-            isNullable: false
           },
           {
             name: 'date',
             type: 'timestamp with time zone',
-            isNullable: false,
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()'
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()'
           }
         ]
       })
