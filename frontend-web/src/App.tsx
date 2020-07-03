676910de5a3138195d32d4fb5a4b45f2934c22ab
juliomchado <juliocarlos00@hotmail.com>
2020-07-03 03:06:51 +0100

    Page login completed

--- frontend-web/src/App.tsx
+++ frontend-web/src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 
+import SignIn from './pages/SigIn';
 import GlobalStyle from './styles/global';
 
 const App: React.FC = () => (
   <>
-    <h1>Hello World</h1>
+    <SignIn />
     <GlobalStyle />
   </>
 );
--- frontend-web/src/styles/global.ts
+++ frontend-web/src/styles/global.ts
@@ -4,7 +4,7 @@ export default createGlobalStyle`
   *{
     margin: 0;
     padding: 0;
-    box-sizing: 0;
+    box-sizing: border-box;
     outline: 0;
   }
 
@@ -23,7 +23,7 @@ export default createGlobalStyle`
     font-weight: 500;
   }
 
-  button: {
+  button{
     cursor: pointer;
   }
 `;
