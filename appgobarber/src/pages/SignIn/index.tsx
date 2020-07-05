b03fb2a96144455a261a2b823475af3efd585d3c
juliomchado <juliocarlos00@hotmail.com>
2020-07-05 22:47:49 +0100

    Import Image

--- appgobarber/src/pages/SignIn/index.tsx
+++ appgobarber/src/pages/SignIn/index.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Image } from 'react-native';
 
 import { Container } from './styles';
 
+import logoImg from '../../assets/logo.png';
+
 const SignIn: React.FC = () => (
-  <Container />
+  <Container>
+    <Image source={logoImg} />
+  </Container>
 
 );
 
