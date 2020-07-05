e90cc090d5b1c22a5171beb19c9b1c75ba384f77
juliomchado <juliocarlos00@hotmail.com>
2020-07-05 01:33:16 +0100

    Logout hook created

--- frontend-web/src/hooks/AuthContext.tsx
+++ frontend-web/src/hooks/AuthContext.tsx
@@ -10,6 +10,7 @@ interface SignInCredentials {
 interface AuthContextData {
   user: object;
   signIn(credentials: SignInCredentials): Promise<void>;
+  signOut(): void;
 }
 
 interface AuthState {
@@ -48,8 +49,15 @@ const AuthProvider: React.FC = ({ children }) => {
     setData({ token, user });
   }, []);
 
+  const signOut = useCallback(() => {
+    localStorage.removeItem('@GoBarber:token');
+    localStorage.removeItem('@GoBarber:user');
+
+    setData({} as AuthState);
+  }, []);
+
   return (
-    <AuthContext.Provider value={{ user: data.user, signIn }}>
+    <AuthContext.Provider value={{ user: data.user, signIn, signOut }}>
       {children}
     </AuthContext.Provider>
   );
