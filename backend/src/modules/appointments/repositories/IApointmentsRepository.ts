81b2fb71510c51b7ad34662f06f0a658927dedbc
juliomchado <juliocarlos00@hotmail.com>
2020-07-23 15:01:24 +0100

    rewritten repository

--- backend/src/modules/appointments/repositories/IApointmentsRepository.ts
+++ backend/src/modules/appointments/repositories/IApointmentsRepository.ts
@@ -1,5 +1,8 @@
 import Appointment from '../infra/typeorm/entities/Appointment';
 
+import ICreateAppointmentDTO from '../dtos/ICreateApppointmentDTO';
+
 export default interface IAppointmentsRepository {
-  findByDate(date: Date): Promise<Appointment | undefined>;
+  create(data: ICreateAppointmentDTO): Promise<Appointment>;
+  findByDate(data: Date): Promise<Appointment | undefined>;
 }
--- backend/src/modules/appointments/services/CreateAppointmentService.ts
+++ backend/src/modules/appointments/services/CreateAppointmentService.ts
@@ -14,6 +14,7 @@ interface IRequest {
 class CreateAppointmentService {
   public async execute({ date, provider_id }: IRequest): Promise<Appointment> {
     const appointmentsRepository = getCustomRepository(AppointmentsRepository);
+
     const appointmentDate = startOfHour(date);
 
     const findAppointmentInSameDate = await appointmentsRepository.findByDate(
@@ -24,12 +25,10 @@ class CreateAppointmentService {
       throw new AppError('This appointment is already booked');
     }
 
-    const appointment = appointmentsRepository.create({
+    const appointment = await appointmentsRepository.create({
       provider_id,
       date: appointmentDate,
     });
-
-    await appointmentsRepository.save(appointment);
 
     return appointment;
   }
