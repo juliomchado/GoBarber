bc3ef4701ff0075ba8ed443b3ed5ac195f68038e
juliomchado <juliocarlos00@hotmail.com>
2020-07-23 15:25:50 +0100

    Apply dependence inversion D (SOLID)

--- backend/src/modules/appointments/infra/http/routes/appointment.routes.ts
+++ backend/src/modules/appointments/infra/http/routes/appointment.routes.ts
@@ -1,6 +1,5 @@
 import { Router } from 'express';
 import { parseISO } from 'date-fns';
-import { getCustomRepository } from 'typeorm';
 
 import AppointmentsRepository from '@modules/appointments/infra/typeorm/repositories/AppointmentsRepository';
 import CreateAppointmentService from '@modules/appointments/services/CreateAppointmentService';
@@ -8,22 +7,24 @@ import CreateAppointmentService from '@modules/appointments/services/CreateAppoi
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 
 const appointmentsRouter = Router();
+const appointmentsRepository = new AppointmentsRepository();
 
 appointmentsRouter.use(ensureAuthenticated);
 
-appointmentsRouter.get('/', async (request, response) => {
-  const appointmentsRepository = getCustomRepository(AppointmentsRepository);
-  const appointmets = await appointmentsRepository.find();
+// appointmentsRouter.get('/', async (request, response) => {
+//   const appointmets = await appointmentsRepository.find();
 
-  return response.json(appointmets);
-});
+//   return response.json(appointmets);
+// });
 
 appointmentsRouter.post('/', async (request, response) => {
   const { provider_id, date } = request.body;
 
   const parsedDate = parseISO(date);
 
-  const createAppointment = new CreateAppointmentService();
+  const createAppointment = new CreateAppointmentService(
+    appointmentsRepository,
+  );
 
   const appointment = await createAppointment.execute({
     date: parsedDate,
