2e555de8f4898f7852fbf26cc6b9c3e183b35194
juliomchado <juliocarlos00@hotmail.com>
2020-08-08 00:26:19 +0100

    Serialization cache fixed

--- backend/src/modules/appointments/services/ListProviderAppointmentsService.ts
+++ backend/src/modules/appointments/services/ListProviderAppointmentsService.ts
@@ -1,6 +1,7 @@
 import { inject, injectable } from 'tsyringe';
 
 import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
+import { classToClass } from 'class-transformer';
 import Appointment from '../infra/typeorm/entities/Appointment';
 
 import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
@@ -44,7 +45,7 @@ class ListProviderAppointmentsService {
         },
       );
 
-      await this.cacheProvider.saveCache(cacheKey, appointments);
+      await this.cacheProvider.saveCache(cacheKey, classToClass(appointments));
     }
 
     return appointments;
