9c34ffe33c496c8f1719fa104689b364f42f7aa4
juliomchado <juliocarlos00@hotmail.com>
2020-08-12 23:16:54 +0100

    routes and pages created

---
+++ appgobarber/src/pages/AppointmentCreated/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { View } from 'react-native';
+
+const AppointmentCreated: React.FC = () => (
+  <View />
+);
+
+export default AppointmentCreated;
---
+++ appgobarber/src/pages/Profile/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { View } from 'react-native';
+
+const Profile: React.FC = () => (
+  <View />
+);
+
+export default Profile;
